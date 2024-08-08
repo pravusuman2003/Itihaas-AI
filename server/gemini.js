dc0c1578c1100ee602140e20a2f59f4781940bd9
Pravu Suman Mohanty <pravusuman2003@gmail.com>
2024-08-08 13:14:18 +0530

    Optimised the Server

--- server/gemini.js
+++ server/gemini.js
@@ -3,21 +3,23 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-async function run(year) {
+async function run(year,interest) {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
   const prompt =
-    "Give me some Significant events that occured in the year " +
+    "Give me 3 most Significant events that occured in the year " +
     year +
-    " in json format as if you are a server and responding to a client";
-
+    " in the field of "
+    +interest+
+    " in json format containing title and description";
+  console.log("Asking Gemini");
   const result = await model.generateContent(prompt);
   const response = await result.response;
   let text = response.text();
-  text = text.substring(7, text.length - 4);
-  const obj = JSON.parse(text);
-  return obj;
+  text=text.substring(text.indexOf('['),text.indexOf(']')+1);
+  console.log("Got response");
+  return JSON.parse(text);
 }
 
 module.exports = { run };
--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const { run } = require("./gemini.js");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+
+app.get("/:year?/:interest", async function (req, res) {
+  try{
+    const obj = await run(req.params.year,req.params.interest);
+    res.json(obj);
+    console.log("data sent to client");
+  }
+  catch(error){
+    res.status(500).send("error : invalid response from gemini");
+  }
 });
 
-app.get("/:year?", async function (req, res) {
-  const obj = await run(req.params.year);
-  res.send(obj);
+app.listen(3000, ()=>{
+  console.log("server started successfully");
 });
-
-app.listen(3000);
-const { run } = require("./gemini.js");
