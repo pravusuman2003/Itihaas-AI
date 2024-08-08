6c66f1e16f6b45f414484873b25046aead8102b2
Pravu Suman Mohanty <pravusuman2003@gmail.com>
2024-08-08 16:27:56 +0530

    Started working on frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,44 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [year,setYear]=useState('');
+  const [interest,setInterest]=useState('');
+  const [result,setResult]=useState('');
+
   const SubmitForm= async(e) => {
+
     e.preventDefault();
+    
+    let url=`http://localhost:3000`;
+    if(year) url+=`/${year}`;
+    if(interest) url+=`/${interest}`;
+    
     try{
-      const res= await fetch()
+      const response= await fetch(url);
+      if(!response.ok)
+        throw new Error();
+      else
+        console.log("fetch successfull");
+      const data= await response.json();
+      setResult(data);
+     }
+    catch{
+      console.log("fetching unsuccessful");
+      setResult(error.message);
     }
-  }
+  };
 
   return (
     <>
-    <form action="" onSubmit={SubmitForm}>
-      <input type="number" name="" id="" />
-      <button type="submit">Submit</button>
-      <div>
-        Response: {JSON.stringify(response)}
-      </div>
-    </form>
-      
+      <form action="" onSubmit={SubmitForm}>
+        <input type="number" id='year' value={year} onChange={(e)=> setYear(e.target.value)} placeholder='Enter Year'/>
+        <input type="text" id='interest' value={interest} onChange={(e)=> setInterest(e.target.value)} placeholder='Enter your interests'/>
+        <button type="submit" >Go back in time</button>
+        <div>
+          {result && <pre>{JSON.stringify(result, null, 2)}</pre>}
+        </div>
+      </form>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { run } = require("./gemini.js");
 
 const app = express();
+
+app.use(cors());
 
 
 app.get("/:year?/:interest", async function (req, res) {
   try{
     const obj = await run(req.params.year,req.params.interest);
-    res.json(obj);
+    res.send(obj);
     console.log("data sent to client");
   }
   catch(error){
