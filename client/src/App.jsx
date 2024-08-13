56ddfd94154281df77801c480e5e24387da3d288
Pravu Suman Mohanty <pravusuman2003@gmail.com>
2024-08-13 20:37:47 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     if(interest) url+=`/${interest}`;
     
     try{
-      const response= await fetch(url);
+      const response= await fetch(url,{ mode: 'no-cors' });
       if(!response.ok)
         throw new Error();
       else
